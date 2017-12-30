22e95167b5ee226df031715b6c79d436f2d44452
Risingvirtue <risingvirtue@berkeley.edu>
2017-12-29 22:13:45 -0800

    url shortener works

--- url.js
+++ url.js
@@ -12,17 +12,30 @@ var links = {};
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', function(req, res) {
-	res.send(__dirname + '/home.html');
+	res.sendFile(path.join(__dirname + '/home.html'));
 });
 
 // set the home page route
 app.get('/*', function(req, res) {
-	var l = req.params[0];
-	if (l in links) {
-		res.redirect((links[i]["original_url"]);
-	} else {
+	var r = req.params[0];
+	var contains = false;
+	for (num in links) {
+		var l = links[num]
+		if (l.original_url == r) {
+			res.send(l);
+			contains = true;
+		} else if (num == r) {
+			res.writeHead(301,
+			{Location: l.original_url}
+			);
+			res.end();
+			contains = true;
+		}
+	}
+	if (!contains) {
 		var rand = getNum();
-		links[rand] = {"original_url": l, "short_url": "https://obscure-island-90192.herokuapp.com/" + rand};
+		links[rand] = {"original_url": r, "short_url": "https://obscure-island-90192.herokuapp.com/" + rand};
+		res.send(links[rand]);
 	}
 });
 
